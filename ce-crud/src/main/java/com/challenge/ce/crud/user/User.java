bcfe1db53f79803f1fb0132d99733f3e054ee6eb
Erik <Erik>
2017-01-17 00:19:16 -0200

    001 #comment Criando repositorio geral do projeto e adicionando configuracoes iniciais do ce-crud

---
+++ ce-crud/src/main/java/com/challenge/ce/crud/user/User.java
@@ -0,0 +1,22 @@
+/*
+ * @(#)User.java 1.0 16/01/2017
+ *
+ * Copyright (c) 2016, Erik Paula. All rights reserved. 
+ */
+package com.challenge.ce.crud.user;
+
+import javax.persistence.Entity;
+
+/**
+ * A classe <code>User</code> representa um usuario que possui uma conta a ser
+ * utilizada no caixa eletronico. Um objeto <code>User</code> passa a existir
+ * no momento em que o mesmo eh inserido no sistema atraves da tela de cadastro
+ * de usuarios.
+ *
+ * @author Erik Paula
+ * @version 1.0 16/01/2017
+ */
+@Entity
+public class User {
+
+}
